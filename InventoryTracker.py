8a77b070b9b77d1dea820e20c91b81d09e12d1a3
aman <aman.bansal1@dcmail.ca>
2026-04-22 13:52:15 -0400

    Initial commit - Final Exam AIDI 2004 - Aman Bansal

---
+++ InventoryTracker.py
@@ -0,0 +1,10 @@
+﻿# Final Exam AIDI 2004 - Aman Bansal
+
+class InventoryTracker:
+    def __init__(self):
+        self.inventory = {}
+
+    def addItem(self, name, quantity):
+        """Add a new item to the inventory with the given quantity."""
+        self.inventory[name] = quantity
+        print(f"Added '{name}' with quantity {quantity}.")
